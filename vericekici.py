fe93a12d59f2b5d8cc310c89c749e38404aef3a0
hackafein <42136880+hackafein@users.noreply.github.com>
2019-09-03 21:43:09 +0300

    Update vericekici.py

--- vericekici.py
+++ vericekici.py
@@ -33,13 +33,19 @@ def get_vendors():
         sicaklik2=son_degerler[1]
         sicaklik3=son_degerler[2]
         sicaklik4=son_degerler[3]
+        gerilim1=son_degerler[4]
+        gerilim2=son_degerler[5]
+        gerilim3=son_degerler[6]
+        gerilim4=son_degerler[7]
+        batarya=son_degerler[8]
+        hiz=son_degerler[9]
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
         if conn is not None:
             conn.close()
-    return sicaklik1,sicaklik2,sicaklik3,sicaklik4
+    return sicaklik1,sicaklik2,sicaklik3,sicaklik4,gerilim1,gerilim2,gerilim3,gerilim4,batarya,hiz
 class Utku:
     def utku(self):
-        (self.sicaklik1, self.sicaklik2, self.sicaklik3,self.sicaklik4) = get_vendors()
-        return (self.sicaklik1, self.sicaklik2, self.sicaklik3,self.sicaklik4)
+        (self.sicaklik1, self.sicaklik2, self.sicaklik3,self.sicaklik4,self.gerilim1,self.gerilim2,self.gerilim3,self.gerilim4,self.batarya,self.hiz) = get_vendors()
+        return (self.sicaklik1, self.sicaklik2, self.sicaklik3,self.sicaklik4,self.gerilim1,self.gerilim2,self.gerilim3,self.gerilim4,self.batarya,self.hiz)
