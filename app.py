c2b335ab4de6cf135297a6413a7e9bf6fdd096c9
hackafein <42136880+hackafein@users.noreply.github.com>
2019-09-03 18:39:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -141,8 +141,8 @@ if __name__ == '__main__':
     except ValueError:
         PORT = 80
     #Sinif().sinif()
-    app.run(HOST, PORT)
-    #socketio.run(app, debug=True)
+    #app.run(HOST, PORT)
+    socketio.run(app, debug=True)
 
 
 ##################################################################################################################
